b164dc73f923aa2240f86adb517f101cf5276bc6
Rafa <rafahuelin@gmail.com>
2019-02-10 08:55:35 +0100

    fix small details that weren't working

--- src/blog/models.py
+++ src/blog/models.py
@@ -7,6 +7,7 @@ from star_ratings.models import Rating
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -16,26 +17,24 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=None)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name+' - '+self.category.name
 
 
 class Product(models.Model):
-	name  = models.CharField(max_length=100)
-	date  = models.DateTimeField(auto_now_add=True)
-	thumb = models.ImageField(default="default.png",blank=True)
-	subcategory = models.ForeignKey(SubCategory,on_delete=models.CASCADE,default=None)
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default="default.png", blank=True)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, default=None)
 
-		
-	def __str__(self):
-		return self.name	
+    def __str__(self):
+        return self.name
 
 
 class RPros(models.Model):
     text = models.CharField(max_length=100)
-    
 
     def __str__(self):
         return self.text
@@ -43,49 +42,43 @@ class RPros(models.Model):
 
 class Cons(models.Model):
     text = models.CharField(max_length=100)
-    
 
     def __str__(self):
         return self.text
 
 
 class Review(models.Model):
-	title = models.CharField(max_length=100)
-	slug  = models.SlugField()
-	body  = models.TextField()
-	date  = models.DateTimeField(auto_now_add=True)
-	product = models.ForeignKey(Product,on_delete=models.CASCADE,default=None)
-	author = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
-	video_url = EmbedVideoField(default="https://www.youtube.com/watch?v=QohH89Eu5iM")
-	rpros = models.ManyToManyField(RPros)
-	rcons = models.ManyToManyField(Cons)
-	comfort=models.IntegerField(default=1,validators=[MaxValueValidator(100), MinValueValidator(1)],null=True)
-	price=models.IntegerField(default=1,validators=[MaxValueValidator(100), MinValueValidator(1)],null=True)
-	availability=models.IntegerField(default=1,validators=[MaxValueValidator(100), MinValueValidator(1)],null=True )
-	support=models.IntegerField(default=1,validators=[MaxValueValidator(100), MinValueValidator(1)],null=True)
-	views = models.IntegerField(default=0)
-	rating = models.ForeignKey(Rating,on_delete=models.CASCADE,default=0,null=True)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    video_url = EmbedVideoField(default="https://www.youtube.com/watch?v=QohH89Eu5iM")
+    rpros = models.ManyToManyField(RPros)
+    rcons = models.ManyToManyField(Cons)
+    comfort = models.IntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(1)], null=True)
+    price = models.IntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(1)], null=True)
+    availability = models.IntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(1)], null=True)
+    support = models.IntegerField(default=1, validators=[MaxValueValidator(100), MinValueValidator(1)], null=True)
+    views = models.IntegerField(default=0)
+    rating = models.ForeignKey(Rating, on_delete=models.CASCADE, default=0, null=True)
 
+    def __str__(self):
+        return self.title
 
-    
-    
-
-	def __str__(self):
-		return self.title	
-
-		#function to return post text only 50 characters
-	def snippet(self):
-		return self.body[:50]+'...'	
-
+        # function to return post text only 50 characters
+    def snippet(self):
+        return self.body[:50]+'...'
 
 
 class UserReview(models.Model):
-	review = models.ForeignKey(Review,on_delete=models.CASCADE,default=None)
-	rating = models.IntegerField(default=0,validators=[MaxValueValidator(5), MinValueValidator(1)])
-	comment= models.TextField()
-	name = models.CharField(max_length=50)
-	email = models.EmailField()
-	date  = models.DateTimeField(auto_now_add=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, default=None)
+    rating = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(1)])
+    comment = models.TextField()
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    date = models.DateTimeField(auto_now_add=True)
 
-	def __str__(self):
-		return self.name+' - '+self.review.title
+    def __str__(self):
+        return self.name+' - '+self.review.title
